a39dff12279b59469926729d2fda9b2ec66a8cb1
winkloid <winkler.oliver@posteo.net>
2021-04-19 18:48:30 +0200

    :sparkles: Added navigation from welcome to home fragment via button, :bug: discovered bug when using navigation menu to call help fragment - when going back, the home fragment is shown every time

--- app/src/main/java/de/tuchemnitz/armadillogin/ui/welcome/WelcomeFragment.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/ui/welcome/WelcomeFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import de.tuchemnitz.armadillogin.R
+import de.tuchemnitz.armadillogin.databinding.FragmentHelpBinding
+import de.tuchemnitz.armadillogin.databinding.FragmentWelcomeBinding
+import de.tuchemnitz.armadillogin.ui.help.HelpViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +23,24 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class WelcomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var binding: FragmentWelcomeBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val fragmentBinding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            welcomeFragment = this@WelcomeFragment
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WelcomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WelcomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun goToNextView() {
+        findNavController().navigate(R.id.action_navigation_welcome_to_navigation_home)
     }
 }
