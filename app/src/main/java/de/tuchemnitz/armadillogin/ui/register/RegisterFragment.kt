28c8cfb0816b9785a7f5e43f02b7eface288d726
winkloid <winkler.oliver@posteo.net>
2021-04-20 15:15:38 +0200

    :sparkles: Added register fragment #1

--- app/src/main/java/de/tuchemnitz/armadillogin/ui/register/RegisterFragment.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/ui/register/RegisterFragment.kt
@@ -1,30 +1,26 @@
-package de.tuchemnitz.armadillogin.ui.registerlogin
+package de.tuchemnitz.armadillogin.ui.register
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import de.tuchemnitz.armadillogin.R
-import de.tuchemnitz.armadillogin.databinding.FragmentHelpBinding
 import de.tuchemnitz.armadillogin.databinding.FragmentRegisterLoginBinding
-import de.tuchemnitz.armadillogin.ui.help.HelpViewModel
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RegisterLoginFragment.newInstance] factory method to
+ * Use the [RegisterFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RegisterLoginFragment : Fragment() {
-
-    private var binding: FragmentRegisterLoginBinding? = null
+class RegisterFragment : Fragment() {
+    private var binding: FragmentRegisterBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentBinding = FragmentRegisterLoginBinding.inflate(inflater, container, false)
+        val fragmentBinding = FragmentRegisterBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
     }
@@ -32,7 +28,7 @@ class RegisterLoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
-            registerLoginFragment = this@RegisterLoginFragment
+            registerFragment = this@RegisterFragment
         }
     }
 }
