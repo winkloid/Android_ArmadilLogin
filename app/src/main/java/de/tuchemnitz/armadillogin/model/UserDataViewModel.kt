750393dde7ad10cabf78db8995f8c0b9e8245435
winkloid <winkler.oliver@posteo.net>
2021-04-27 15:52:40 +0200

    :construction: Added some values in UserDataViewModel which will be used for all registration and login data

--- app/src/main/java/de/tuchemnitz/armadillogin/model/UserDataViewModel.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/model/UserDataViewModel.kt
@@ -1,2 +1,20 @@
 package de.tuchemnitz.armadillogin.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserDataViewModel : ViewModel() {
+    private var _firstname = MutableLiveData<String>("Test")
+    val firstname: LiveData<String> = _firstname
+
+    private var _lastname = MutableLiveData<String>("User")
+    val lastname: LiveData<String> = _lastname
+
+    private var _email = MutableLiveData("klaus.uhr@tu-chemnitz.de")
+    val email: LiveData<String> = _email
+
+    private var _username = MutableLiveData("username")
+    val username: LiveData<String> = _username
+    
+}
