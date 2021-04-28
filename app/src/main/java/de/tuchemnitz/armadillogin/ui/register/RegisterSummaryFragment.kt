d3f9bdf7daf93d38466cf4e86053b292ebd6bb58
winkloid <winkler.oliver@posteo.net>
2021-04-28 14:30:53 +0200

    :construction: Added fragment for the user to verify his or her inputs

--- app/src/main/java/de/tuchemnitz/armadillogin/ui/register/RegisterSummaryFragment.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/ui/register/RegisterSummaryFragment.kt
@@ -1,4 +1,50 @@
 package de.tuchemnitz.armadillogin.ui.register
 
-class RegisterSummaryFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import de.tuchemnitz.armadillogin.R
+import de.tuchemnitz.armadillogin.databinding.FragmentRegisterBinding
+import de.tuchemnitz.armadillogin.model.ArmadilloViewModel
+import de.tuchemnitz.armadillogin.model.FragmentStatus
+import de.tuchemnitz.armadillogin.model.UserDataViewModel
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterSummaryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterSummaryFragment : Fragment() {
+    private var binding: FragmentRegisterBinding? = null
+    private val sharedViewModel: ArmadilloViewModel by activityViewModels()
+    private val userViewModel: UserDataViewModel by activityViewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentRegisterBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            registerSummaryFragment = this@RegisterSummaryFragment
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sharedViewModel.setFragmentStatus(FragmentStatus.REGISTER1)
+    }
 }
