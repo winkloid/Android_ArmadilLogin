1b55275ea48f8b4a019c671d8956ebc5ff5504b4
winkloid <winkler.oliver@posteo.net>
2021-05-03 17:48:25 +0200

    :construction: Added gradle dependencies for fido2 support

--- app/src/main/java/de/tuchemnitz/armadillogin/fido2api/AddHeaderInterceptor.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/fido2api/AddHeaderInterceptor.kt
@@ -1,5 +1,8 @@
 package de.tuchemnitz.armadillogin.fido2api
 
+import okhttp3.Interceptor
+import okhttp3.Response
+
 class AddHeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return chain.proceed(
