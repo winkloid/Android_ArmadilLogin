5f29ac51437682c9958669228de574c407cb983c
winkloid <winkler.oliver@posteo.net>
2021-05-03 18:08:25 +0200

    :construction: Added Fido2Api extension files provided in this repository under Apache 2.0 license: https://github.com/googlecodelabs/fido2-codelab

--- app/src/main/java/de/tuchemnitz/armadillogin/fido2api/ApiException.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/fido2api/ApiException.kt
@@ -16,15 +16,4 @@
 
 package de.tuchemnitz.armadillogin.fido2api
 
-import okhttp3.Interceptor
-import okhttp3.Response
-
-class AddHeaderInterceptor : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        return chain.proceed(
-                chain.request().newBuilder()
-                        .header("X-Requested-With", "XMLHttpRequest")
-                        .build()
-        )
-    }
-}
+class ApiException(message: String) : RuntimeException(message)
