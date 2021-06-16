61e551089ef688bea9f32fc5ad20eaeaa390ab0c
winkloid <winkler.oliver@posteo.net>
2021-06-16 14:59:15 +0200

    :construction: Added user data fragment implementation and methods to store gender and technical experience

--- app/src/main/java/de/tuchemnitz/armadillogin/model/StudyUserDataViewModel.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/model/StudyUserDataViewModel.kt
@@ -1,16 +1,61 @@
 package de.tuchemnitz.armadillogin.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StudyUserDataViewModel: ViewModel() {
     // time measurement feature
-    var userStartTime: Long? = null
-    var userFinishedTime: Long? = null
-    var userTime: Long? = null
+    var userStartTime: Long = 0
+    var userFinishedTime: Long = 0
+    var userTime: Long = 0
 
     fun calculateUserTime() {
-        if (userFinishedTime != null && userStartTime != null) {
-            userTime = userFinishedTime!! - userStartTime!!
-        }
+        userTime = userFinishedTime - userStartTime
+    }
+
+    // personal user data for study and statistics
+    /**
+     * Represents the age of the user.
+     * null - not specified
+     * else - the actual age
+     */
+    private var _age = MutableLiveData<Int?>(null)
+    val age: LiveData<Int?> = _age
+
+    /**
+     * Represents the gender of the user.
+     * null - not specified
+     * 0 - Female
+     * 1 - Male
+     * 2 - Diverse
+     */
+    private var _gender = MutableLiveData<Int?>(null)
+    val gender: LiveData<Int?> = _gender
+
+    /**
+     * Sets the gender specified by the user immediately when user selects it from radio group in UserDataFragment.
+     */
+    fun setGender(genderInput: Int?) {
+        _gender.value = genderInput
+    }
+
+    /**
+     * Represents technical experience of the user.
+     * null - not specified
+     * 1 - none
+     * 2 - little
+     * 3 - average
+     * 4 - much
+     * 5 - expert level
+     */
+    private var _technicalExperience = MutableLiveData<Int?>(null)
+    val technicalExperience: LiveData<Int?> = _technicalExperience
+
+    /**
+     * Sets the gender specified by the user immediately when user selects it from radio group in UserDataFragment.
+     */
+    fun setTechnicalExperience(technicalExperienceInput: Int?) {
+        _technicalExperience.value = technicalExperienceInput
     }
 }
