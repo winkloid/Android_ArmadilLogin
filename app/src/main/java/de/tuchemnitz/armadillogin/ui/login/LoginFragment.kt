7bc07a793163ffb44930e80ba8082a2ccc4699ca
winkloid <winkler.oliver@posteo.net>
2021-07-01 15:44:57 +0200

    :bulb: Added comments for ui.login package.

--- app/src/main/java/de/tuchemnitz/armadillogin/ui/login/LoginFragment.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/ui/login/LoginFragment.kt
@@ -16,14 +16,35 @@ import de.tuchemnitz.armadillogin.model.FragmentStatus
 import de.tuchemnitz.armadillogin.model.UserDataViewModel
 
 /**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A [Fragment] subclass used as login fragment.
+ *
+ * In this fragment the user is asked for the user name that he/she registered before.
+ * After the input is confirmed, the application checks whether the input is valid and then sends the username to the server to have a session ID generated.
  */
 class LoginFragment : Fragment() {
 
+    /**
+     * Binding object instance.
+     *
+     * Refers to fragment_login.xml
+     */
     private var binding: FragmentLoginBinding? = null
+
+    /**
+     * Shared [ArmadilloViewModel] for fragment status and settings.
+     *
+     * Manages the [FragmentStatus] value to display adapted resources in the Help tab. It also manages some variables to store user settings like color mode or font settings.
+     */
     private val sharedViewModel: ArmadilloViewModel by activityViewModels()
+
+    /**
+     * Shared [UserDataViewModel] for storing personal data during registration process.
+     *
+     * This data is not stored after the app is closed, nor does it serve the purpose of data collection at all.
+     * The data is only used to make the registration process as genuine as possible by requesting data from the user that would also be requested during a real registration process.
+     * This data includes, among other things, name, e-mail address and username.
+     * In addition, the ViewModel interfaces with the FIDO2 API.
+     */
     private val userViewModel: UserDataViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -35,6 +56,12 @@ class LoginFragment : Fragment() {
         return fragmentBinding.root
     }
 
+    /**
+     * Call super.onViewCreated() and activate data binding.
+     *
+     * Use this [Fragment] subclass as lifecycleOwner for data binding and assign the other variables to use them in fragment_login.xml.
+     * You can find these variables declared in the <data> section of fragment_login.xml.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
@@ -45,6 +72,12 @@ class LoginFragment : Fragment() {
         }
     }
 
+    /**
+     * Set [FragmentStatus] in [sharedViewModel] and announce [LoginFragment] to screen readers when it is resumed.
+     *
+     * Changing the fragment status is necessary to display content in help section that is adapted to [LoginFragment].
+     * Announcing [LoginFragment] when it is resumed increases the accessibility of the application.
+     */
     override fun onResume() {
         super.onResume()
         sharedViewModel.setFragmentStatus(FragmentStatus.LOGIN)
@@ -52,9 +85,10 @@ class LoginFragment : Fragment() {
     }
 
     /**
-     * Is called via data binding when the Button is Pressed
+     * Check whether the input value is valid and after this send the username to the FIDO2 server.
+     *
      * Calls [checkValues] first to check whether the user entered a valid username
-     * Calls [sendUserName] after this to send the provided username to the server and to navigate using [R.id.action_navigation_login1_to_navigation_login_key] action
+     * Calls [sendUserName] after this to send the provided username to the server and to navigate to next [Fragment] using [R.id.action_navigation_login1_to_navigation_login_key] action.
      */
     fun goToNextView() {
         val valuesCorrect = checkValues()
@@ -64,8 +98,10 @@ class LoginFragment : Fragment() {
     }
 
     /**
-     * Check whether the username provided by the user is a string that is not null and not blank
-     * returns Boolean value to show whether the provided value is acceptable or not
+     * Check whether the username entered by the user is valid.
+     *
+     * Check whether the username provided by the user is a string that is not null and not blank.
+     * @return Boolean value to show whether the provided value is acceptable or not
      */
     private fun checkValues(): Boolean {
         val username = binding?.loginInputUsernameEditText?.text.toString()
@@ -82,9 +118,11 @@ class LoginFragment : Fragment() {
     }
 
     /**
+     * Send username to FIDO2 server to have a session ID generated.
+     *
      * Uses sendUsername method from [userViewModel] to send the username provided by the user to the fido2/webauthn server.
-     * After this is done, the user is redirected to LoginKeyFragment
-     * Calls [userViewModel] method to reset the bool that ensures that the username() method in AuthRepository is always executed before password() method is executed when user registers
+     * After this is done, the user is redirected to [LoginKeyFragment].
+     * Calls [userViewModel] setUsernameBeforePassword method to reset the bool that ensures that the username() method in AuthRepository is always executed before password() method.
      */
     private fun sendUserName() {
         Toast.makeText(
--- app/src/main/java/de/tuchemnitz/armadillogin/ui/login/LoginKeyFragment.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/ui/login/LoginKeyFragment.kt
@@ -20,11 +20,15 @@ import de.tuchemnitz.armadillogin.model.FragmentStatus
 import de.tuchemnitz.armadillogin.model.StudyUserDataViewModel
 import de.tuchemnitz.armadillogin.model.UserDataViewModel
 import de.tuchemnitz.armadillogin.ui.observeOnce
+import de.tuchemnitz.armadillogin.ui.register.RegisterErrorFragment
+import de.tuchemnitz.armadillogin.ui.register.RegisterKeyFragment
 
 /**
- * A simple [Fragment] subclass.
- * Use the [LoginKeyFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A [Fragment] subclass used as login key fragment.
+ *
+ * In this fragment, the user is informed about how the login process works with his FIDO2 key.
+ * This is barely different from the registration of the key.
+ * There is also a button in this fragment to start the login process with the FIDO2 key.
  */
 class LoginKeyFragment : Fragment() {
 
@@ -33,9 +37,35 @@ class LoginKeyFragment : Fragment() {
         private const val LOG_TAG = "FIDO2_REGISTERKEY"
     }
 
+    /**
+     * Binding object instance.
+     *
+     * Refers to fragment_login_key.xml
+     */
     private var binding: FragmentLoginKeyBinding? = null
+
+    /**
+     * Shared [ArmadilloViewModel] for fragment status and settings.
+     *
+     * Manages the [FragmentStatus] value to display adapted resources in the Help tab. It also manages some variables to store user settings like color mode or font settings.
+     */
     private val sharedViewModel: ArmadilloViewModel by activityViewModels()
+
+    /**
+     * Shared [UserDataViewModel] for storing personal data during registration process.
+     *
+     * This data is not stored after the app is closed, nor does it serve the purpose of data collection at all.
+     * The data is only used to make the registration process as genuine as possible by requesting data from the user that would also be requested during a real registration process.
+     * This data includes, among other things, name, e-mail address and username.
+     * In addition, the ViewModel interfaces with the FIDO2 API.
+     */
     private val userViewModel: UserDataViewModel by activityViewModels()
+
+    /**
+     * Shared [StudyUserDataViewModel] for study data.
+     *
+     * Saves all data provided by users and time data. It also provides functionality to securely transmit these data to my study database.
+     */
     private val studyUserViewModel: StudyUserDataViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -47,6 +77,12 @@ class LoginKeyFragment : Fragment() {
         return fragmentBinding.root
     }
 
+    /**
+     * Call super.onViewCreated() and activate data binding.
+     *
+     * Use this [Fragment] subclass as lifecycleOwner for data binding and assign the other variables to use them in fragment_login_key.xml.
+     * You can find these variables declared in the <data> section of fragment_login_key.xml.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
@@ -57,6 +93,12 @@ class LoginKeyFragment : Fragment() {
         }
     }
 
+    /**
+     * Set [FragmentStatus] in [sharedViewModel] and announce [LoginKeyFragment] to screen readers when it is resumed.
+     *
+     * Changing the fragment status is necessary to display content in help section that is adapted to [LoginKeyFragment].
+     * Announcing [LoginKeyFragment] when it is resumed increases the accessibility of the application.
+     */
     override fun onResume() {
         super.onResume()
         sharedViewModel.setFragmentStatus(FragmentStatus.LOGIN_KEY)
@@ -64,6 +106,13 @@ class LoginKeyFragment : Fragment() {
     }
 
     // sendLoginRequest and onActivityResult are used as shown in https://github.com/googlecodelabs/fido2-codelab
+    /**
+     * Send a request to server to login with FIDO2 key. Measure system time.
+     *
+     * Measure the system time to get beginning time of login intent.
+     * Send a signIn request to FIDO2 server by calling signInRequest method of [userViewModel] which returns a PendingIntent as LiveData.
+     * Set observer to this method and start login intent as soon as response comes from the server.
+     */
     fun sendLoginRequest() {
         if (studyUserViewModel.userLoginStartTime == null) {
             studyUserViewModel.userLoginStartTime = System.nanoTime()
@@ -81,8 +130,13 @@ class LoginKeyFragment : Fragment() {
         }
     }
 
+    /**
+     * Get result from login intent and show corresponding messages or jump to next [Fragment]
+     * If the [resultCode] contains an error, show a [Toast] message that contains the error message.
+     * If the response signals that the intent was canceled, show a corresponding toast message.
+     * If the response does not contain any errors, measure the [System] time so that userFinishedTime can be stored in [studyUserViewModel]. After this, jump to  [de.tuchemnitz.armadillogin.ui.user.UserOverviewFragment].
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        Log.d(LOG_TAG, "requestCode: $requestCode")
 
         if (requestCode == FIDO2_SIGNIN_REQUEST_CODE) {
             val errorExtra = data?.getByteArrayExtra(Fido.FIDO2_KEY_ERROR_EXTRA)
@@ -106,7 +160,6 @@ class LoginKeyFragment : Fragment() {
                 else -> {
                     if (data != null) {
                         userViewModel.signInResponse(data)
-                        Log.d(LOG_TAG, "$data")
 
                         // inserted by winkloid
                         // observe whether signInResponse already completed and suceeded and if so, go to next fragment
