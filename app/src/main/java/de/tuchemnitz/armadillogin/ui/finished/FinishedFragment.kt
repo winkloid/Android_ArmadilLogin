55fa4aa90f7cd063e0731c65a724facd6d464b40
winkloid <winkler.oliver@posteo.net>
2021-07-02 10:57:32 +0200

    :bulb: Added comments for ui.finished package.

--- app/src/main/java/de/tuchemnitz/armadillogin/ui/finished/FinishedFragment.kt
+++ app/src/main/java/de/tuchemnitz/armadillogin/ui/finished/FinishedFragment.kt
@@ -13,15 +13,28 @@ import de.tuchemnitz.armadillogin.databinding.FragmentFinishedBinding
 import de.tuchemnitz.armadillogin.databinding.FragmentWelcomeBinding
 import de.tuchemnitz.armadillogin.model.ArmadilloViewModel
 import de.tuchemnitz.armadillogin.model.FragmentStatus
-
+import de.tuchemnitz.armadillogin.ui.login.LoginFragment
 
 /**
- * A simple [Fragment] subclass.
- * Use the [FinishedFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A [Fragment] subclass used as finished fragment.
+ *
+ * In this fragment, the user is informed that he/she has successfully completed the study.
+ * In addition, the user is given the option to return to the [WelcomeFragment][de.tuchemnitz.armadillogin.ui.welcome.WelcomeFragment] via a button.
  */
 class FinishedFragment : Fragment() {
+
+    /**
+     * Binding object instance.
+     *
+     * Refers to fragment_finished.xml
+     */
     private var binding: FragmentFinishedBinding? = null
+
+    /**
+     * Shared [ArmadilloViewModel] for fragment status and settings.
+     *
+     * Manages the [FragmentStatus] value to display adapted resources in the Help tab. It also manages some variables to store user settings like color mode or font settings.
+     */
     private val sharedViewModel: ArmadilloViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -33,6 +46,12 @@ class FinishedFragment : Fragment() {
         return fragmentBinding.root
     }
 
+    /**
+     * Call super.onViewCreated() and activate data binding.
+     *
+     * Use this [Fragment] subclass as lifecycleOwner for data binding and assign the other variables to use them in fragment_finished.xml.
+     * You can find these variables declared in the <data> section of fragment_finished.xml.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.apply {
@@ -42,13 +61,23 @@ class FinishedFragment : Fragment() {
         }
     }
 
+    /**
+     * Set [FragmentStatus] in [sharedViewModel] and announce [FinishedFragment] to screen readers when it is resumed.
+     *
+     * Changing the fragment status is necessary to display content in help section that is adapted to [FinishedFragment].
+     * Announcing [FinishedFragment] when it is resumed increases the accessibility of the application.
+     */
     override fun onResume() {
         super.onResume()
-        Log.d("D", "Resuming")
         sharedViewModel.setFragmentStatus(FragmentStatus.FINISHED)
         view?.announceForAccessibility(getString(R.string.finished_accessibility_label))
     }
 
+    /**
+     * Jump back to [WelcomeFragment][de.tuchemnitz.armadillogin.ui.welcome.WelcomeFragment].
+     *
+     * Call [findNavController] to navigate to [WelcomeFragment][de.tuchemnitz.armadillogin.ui.welcome.WelcomeFragment].
+     */
     fun backToWelcome() {
         findNavController().navigate(R.id.action_navigation_finished_to_navigation_welcome)
     }
